18540be6ef62eb3385e339f68de8c8279e34112f
Clemens Radermacher <clemera@posteo.net>
2019-03-31 20:49:54 +0200

    Add swiper support

--- mcfly.el
+++ mcfly.el
@@ -7,19 +7,16 @@
 
 (defun mcfly-back-to-present ()
   (remove-hook 'pre-command-hook 'mcfly-back-to-present t)
-  (cond ((memq this-command '(minibuffer-complete-and-exit
-                              exit-minibuffer))
-         (ignore))
-        ((equal (this-command-keys-vector) (kbd "M-p"))
+  (cond ((equal (this-command-keys-vector) (kbd "M-p"))
          ;; repeat one time to get straight to the first history item
          (setq unread-command-events
                (append unread-command-events
                        (listify-key-sequence (kbd "M-p")))))
-        ((not (= (point) (minibuffer-prompt-end)))
-         (delete-region (point) (point-max)))
-        (t
-         (delete-region (minibuffer-prompt-end)
-                        (point-max)))))
+        ((memq this-command '(self-insert-command))
+         (if (not (= (point) (minibuffer-prompt-end)))
+             (delete-region (point) (point-max))
+           (delete-region (minibuffer-prompt-end)
+                          (point-max))))))
 
 (defun mcfly-time-travel ()
   (when (memq this-command mcfly-commands)
@@ -35,7 +32,18 @@
           (insert (propertize future 'face 'shadow)))
         (add-hook 'pre-command-hook 'mcfly-back-to-present nil t)))))
 
+;; setup code
 (add-hook 'minibuffer-setup-hook #'mcfly-time-travel)
+(with-eval-after-load 'ivy
+  (push (cons 'swiper 'mcfly-swiper)
+        ivy-hooks-alist)
+  (defun mcfly-swiper ()
+    (let ((sym (with-ivy-window
+                 (thing-at-point 'symbol))))
+      (when sym
+        (add-hook 'pre-command-hook 'mcfly-back-to-present nil t)
+        (save-excursion
+          (insert (propertize sym 'face 'shadow)))))))
 
 
 
